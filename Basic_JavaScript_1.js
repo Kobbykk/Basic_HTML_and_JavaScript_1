87ada710d7e15ac2d1cb4816d94dd678f6b7e5db
Kobby236 <edwinaddai.ea@gmail.com>
2024-04-29 19:38:41 -0400

    Initial commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Welcome to my page")
+
+
+function jav_function(){
+	var String = "Kiss me, I'm Irish";
+	var result = String.fontcolor("green");
+	document.getElementById("unhide").innerHTML = result;
+	
+}
