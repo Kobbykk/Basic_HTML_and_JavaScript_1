3d27b02a6082aca0fb6e162c448936d0b1b45bb5
Kobby236 <edwinaddai.ea@gmail.com>
2024-05-06 16:40:45 -0400

    updated code
    added more code - updated

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,9 +1,7 @@
-alert("Welcome to my page")
+alert("Welcome to my page");
 
-
-function jav_function(){
-	var String = "Kiss me, I'm Irish";
-	var result = String.fontcolor("green");
-	document.getElementById("unhide").innerHTML = result;
-	
+function jav_function() {
+  var String = "Kiss me, I'm Irish";
+  var result = String.fontcolor("green");
+  document.getElementById("unhide").innerHTML = result;
 }
